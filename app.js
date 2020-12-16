88a02a45ba2d4de72aa6de5e3bf2f752dd1e728f
Brendan Caldwell <40771777+Brednan@users.noreply.github.com>
2020-12-15 21:04:44 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const yargs = require('yargs')
+const request = require('request')
+const saveWeatherData = require('./saveWeatherData.js')
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=New+York&appid=8d6e5706b17fb3303800fea3091696e1'
+yargs.command('Request', 'get data of city', {
+    city:{
+        type: 'string',
+        demandOption: true
+    }
+}, (argv) => {
+    const cityName = argv.city
+    const cityNameReplaced = cityName.replace(' ', '+')
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityNameReplaced + '&appid=8d6e5706b17fb3303800fea3091696e1'
+    request({url: url, json: true}, (error, data) => {
+        const weatherString = JSON.stringify(data.body.weather)
+        const weatherStringBracket_1stRemove = weatherString.replace('[', '')
+        const weatherStringBracket_2ndRemove = weatherStringBracket_1stRemove.replace(']', '')
+        const weatherJSON = JSON.parse(weatherStringBracket_2ndRemove)
+        saveWeatherData.saveWeather(cityName,data.body.main.temp, data.body.timezone)
+    })
+})
+
+yargs.command('ReadWeather', 'read weather of city listed', {
+    city: {
+        demandOption: true,
+        type: 'string'
+    }},
+    (argv) =>{
+        saveWeatherData.readWeather(argv.name)
+    })
+yargs.command('RemoveWeather', 'Remove selected weather from list', {
+    city:{
+        demandOption: true,
+        type:'string'
+    }},
+    (argv) =>{
+        saveWeatherData.removeWeather(argv.name)
+    })
+yargs.parse()
---
+++ saveWeatherData.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+
+function loadWeather(){
+
+    try{
+        const weather = fs.readFileSync('./weatherData.json')
+        const weatherDataString = weather.toString()
+        return JSON.parse(weatherDataString)
+    }
+    catch(e){
+        console.log(e)
+        return []
+    }
+}
+function saveWeather(name, temperature, timezone){
+    const weather = loadWeather()
+    // const weatherDuplicates = weather.find((w) => {
+    //     return w.
+    // })
+    weather.push({
+        name: name,
+        temperature: temperature,
+        timezone: timezone,
+    })
+    fs.writeFileSync('./weatherData.json', JSON.stringify(weather))
+}
+function readWeather(name){
+    const weather = loadWeather()
+    const weatherRead = weather.find((w) =>{
+        return w.name === name
+    })
+    console.log(weatherRead)
+}
+function removeWeather(name){
+    try{const weather = fs.readFileSync('./weatherData.json')
+    const weatherString = weather.toString()
+    const weatherJSON = JSON.parse(weatherString)
+
+    const newWeatherArray = weatherJSON.filter((w) =>{
+        return w.name !== name
+    })
+    fs.writeFileSync('./weatherData.json', JSON.stringify(newWeatherArray))
+}
+catch(e){
+    console.log(e)
+}
+}
+module.exports = {
+    saveWeather: saveWeather,
+    readWeather: readWeather,
+    removeWeather: removeWeather
+}
